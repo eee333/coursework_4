de2844adc690c3ff4d3c687e89fe4481c7806791
Evgeny Eroshin <eee333@yandex.ru>
2022-02-13 18:59:20 +0300

    Create hero

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, url_for
 from werkzeug.utils import redirect
-
 from equipment import EquipmentData
 from personages import personage_classes
 from utils import load_equipment
@@ -11,6 +10,15 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+def render_choose_template(*args, **kwargs) -> str:
+    return render_template(
+        "hero_choosing.html",
+        classes=personage_classes.values(),
+        equipment=EQUIPMENT,
+        **kwargs,
+    )
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -19,24 +27,14 @@ def index():
 @app.route("/choose-hero", methods=["GET", "POST"])
 def choose_hero():
     if request.method == "GET":
-        return render_template(
-            "hero_choosing.html",
-            header="Выберите героя",
-            classes=personage_classes.values(),
-            equipment=EQUIPMENT
-        )
+        return render_choose_template(header="Выберите героя")
     return redirect(url_for("choose_enemy"))
 
 
 @app.route("/choose-enemy", methods=["GET", "POST"])
 def choose_enemy():
     if request.method == "GET":
-        return render_template(
-            "hero_choosing.html",
-            header="Выберите героя",
-            classes=personage_classes.values(),
-            equipment=EQUIPMENT
-        )
+        return render_choose_template(header="Выберите врага")
     return ""
 
 
---
+++ hero.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from random import randint
+from typing import Type, Optional
+
+from equipment import Weapon, Armor
+from personages import Personage
+
+BASE_STAMINA_PER_ROUND = 0.4
+
+
+class Hero(ABC):
+    def __init__(self, class_: Type[Personage], weapon: Weapon, armor: Armor, name: str):
+        self.class_ = class_
+        self.weapon = weapon
+        self.armor = armor
+        self._stamina = self.class_.max_stamina
+        self._health = self.class_.max_health
+        self.skill_used: bool = False
+        self.name = name
+
+    @property
+    def health(self):
+        return round(self._health, 1)
+
+    @health.setter
+    def health(self, value):
+        self._health = value
+
+    @property
+    def stamina(self):
+        return round(self._stamina, 1)
+
+    @stamina.setter
+    def stamina(self, value) :
+        self._stamina = value
+
+    @property
+    def _total_armor(self)-> float:
+        if self.stamina - self.armor.stamina_per_turn >= 0:
+            return self.armor.defence * self.class_.armor
+        return 0
+
+    def _hit(self, target: Hero) -> Optional[float]:
+        if self.stamina - self.weapon.stamina_per_hit < 0:
+            return None
+        hero_damage = self.weapon.damage * self.class_.attack
+        delta_damage = hero_damage - target._total_armor
+        if delta_damage < 0:
+            return 0
+        self.stamina -= self.weapon.stamina_per_hit
+        return round(delta_damage, 1)
+
+    def take_damage(self, damage: float):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+
+    def use_skill(self) -> Optional[float]:
+        if self.stamina >= self.class_.skill.stamina and not self.skill_used:
+            self.skill_used = True
+            return round(self.class_.skill.damage, 1)
+        return None
+
+    def regenerate_stamina(self):
+        delta_stamina = BASE_STAMINA_PER_ROUND * self.class_.stamina
+        self.stamina += delta_stamina
+        if self.stamina > self.class_.max_stamina:
+            self.stamina = self.class_.max_stamina
+
+    @abstractmethod
+    def hit(self, target: Hero) -> Optional[float]:
+        pass
+
+
+class Enemy(Hero):
+    def hit(self, target: Hero) -> Optional[float]:
+        if randint(1, 100) < 11 and self.stamina >= self.class_.skill.stamina and not self.skill_used:
+            self.use_skill()
